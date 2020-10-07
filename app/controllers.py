aa7245276f3e1295aee5f51c69a869a337f31699
sssemion <ssemi.on@yandex.ru>
2020-10-07 19:28:35 +0500

    Сброс PIN'а

--- app/controllers.py
+++ app/controllers.py
@@ -11,7 +11,7 @@ from app.forms.pin import PinForm
 from app.forms.register import RegisterForm
 from app.services.users import confirm_token, register_from_form, redirect_if_authorized, \
     login_from_form, logout, redirect_if_unauthorized, change_password_from_form, get_myself, \
-    set_pin_from_form, edit_profile_from_form
+    set_pin_from_form, edit_profile_from_form, reset_pin
 
 
 @app.before_request
@@ -34,7 +34,7 @@ def confirm(token):
 def register():
     form = RegisterForm()
     if register_from_form(form):
-        return redirect("/")
+        return redirect("/profile")
     return render_template("register.html", form=form)
 
 
@@ -43,7 +43,7 @@ def register():
 def login():
     form = LoginForm()
     if login_from_form(form):
-        return redirect("/")
+        return redirect("/profile")
     return render_template("login.html", form=form)
 
 
@@ -57,9 +57,11 @@ def logout_():
 @app.route("/pin", methods=["GET", "POST"])
 @redirect_if_unauthorized
 def pin():
+    if g.current_user["is_pin_set"]:
+        reset_pin()
     form = PinForm()
     if set_pin_from_form(form):
-        return redirect("/")
+        return redirect("/profile")
     return render_template("pin.html", form=form)
 
 
@@ -68,7 +70,7 @@ def pin():
 def change_password_():
     form = PasswordForm()
     if change_password_from_form(form):
-        return redirect("/")
+        return redirect("/profile")
     return render_template("password.html", form=form)
 
 
