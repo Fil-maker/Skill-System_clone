c3ece5e68e3a7f6532af9874b7ec969ff5dcf6b0
Star <zubareff10@gmail.com>
2020-10-23 17:20:39 +0500

    фикс контролера (Убрал отсавшуюся логику)

--- app/controllers.py
+++ app/controllers.py
@@ -1,7 +1,5 @@
-import requests
 from flask import render_template, g, session
 from werkzeug.utils import redirect
-import os
 from app import app
 from app.forms.editProfile import EditProfileForm
 from app.forms.eventDates import EditEventDatesForm
@@ -17,8 +15,6 @@ from app.services.events import create_event_from_form, edit_event_information_f
 from app.services.users import confirm_token, register_from_form, redirect_if_authorized, \
     login_from_form, logout, redirect_if_unauthorized, change_password_from_form, get_myself, \
     set_pin_from_form, edit_profile_from_form, reset_pin, only_for_admin
-
-api_url = f"http://{os.environ.get('API_HOST')}:{os.environ.get('API_PORT')}/api"
 
 
 @app.before_request
@@ -100,12 +96,11 @@ def profile():
     return render_template("selfProfile.html", current_user=g.current_user)
 
 
+# TODO: Получить данные
 @app.route("/user/<int:user_id>")
 @redirect_if_unauthorized
 def user_profile(user_id):
-    response = requests.get(f'{api_url}/users/{user_id}')
-    data = response.json()
-    user = data['user']
+    user = []
     return render_template('userProfile.html', user=user)
 
 
@@ -119,6 +114,7 @@ def create_event_():
     return render_template("eventRegister.html", form=form)
 
 
+# TODO: Получить данные
 @app.route("/event/<int:event_id>")
 @redirect_if_unauthorized
 def event_profile(event_id):
@@ -126,6 +122,7 @@ def event_profile(event_id):
     return render_template('eventProfile.html', event=event)
 
 
+# TODO: Получить данные
 @app.route("/event-list")
 @redirect_if_unauthorized
 @only_for_admin
@@ -145,6 +142,7 @@ def edit_event_information_(event_id):
     return render_template("eventInformation.html", form=form)
 
 
+# TODO: Получить данные
 @app.route("/event/<int:event_id>/participants")
 @redirect_if_unauthorized
 @only_for_admin
