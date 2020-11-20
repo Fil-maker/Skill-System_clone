d2f020ffb6c98f792b2c33cc54248cc7cc26da35
Star <zubareff10@gmail.com>
2020-11-21 02:44:56 +0500

    Долгий фикс времени (теперь "ПОЛНОСТЬЮ НА РУССКОМ ЯЗЫКЕ")

--- app/momentjs.py
+++ app/momentjs.py
@@ -8,11 +8,14 @@ class momentjs(object):
         self.timestamp = datetime.datetime.strptime(timestamp, "%Y-%m-%d")
 
     def render(self, format):
-        return Markup("<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (
+        return Markup(f"<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (
             self.timestamp.strftime("%Y-%m-%d"), format))
 
     def format(self, fmt):
         return self.render("format(\"%s\")" % fmt)
+
+    def locale(self, fmt):
+        return Markup(f"<script>\nmoment.locale('{fmt}');\n</script>")
 
     def calendar(self):
         return self.render("calendar()")
