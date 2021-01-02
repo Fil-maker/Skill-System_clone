45764d376c180fba62a60f45d12c27490c5cb3d9
Star <zubareff10@gmail.com>
2021-01-02 06:56:10 +0500

    Теперь назначение и удаление форм работает почти без обновлений страницы (За этот код мне будет стыдно)

--- app/static/js/form.js
+++ app/static/js/form.js
@@ -1,39 +1,80 @@
 $(function(){
-    $('.assign-btn').on("click", function() {
-        var data = this.value;
+
+    function assign(e) {
+        var data = e.value.split(',');
         var href = window.location.href;
         var event_id = href.split("/")[4];
-        $.ajax("/ajax/forms/assign/" + event_id + "/" + data, {
+        $.ajax("/ajax/forms/assign/" + event_id + "/" + data[0], {
             method: 'post',
             dataType: 'json',
 
             success: function(data) {
                 $.jGrowl('', {'header': 'Successfully assigned'});
-                location.reload(); // Заглушка TODO
             },
 
             error: function(data) {
                 $.jGrowl(data["message"], {'header': 'Error'});
+                location.reload();
             }
         });
-    });
+    }
 
-    $('.delete-btn').on("click", function() {
-        var data = this.value;
+    function del(e) {
+        var data = e.value;
         var href = window.location.href;
         var event_id = href.split("/")[4];
-        $.ajax("/ajax/forms/delete/" + event_id + "/" + data, {
+        var suc = false;
+        $.ajax("/ajax/forms/delete/" + event_id + "/" + data[0], {
             method: 'post',
             dataType: 'json',
 
             success: function(data) {
                 $.jGrowl('', {'header': 'Successfully deleted'});
-                location.reload(); // Заглушка TODO
             },
 
             error: function(data) {
                 $.jGrowl(data["message"], {'header': 'Error'});
+                location.reload();
             }
         });
+    }
+
+    $('.assign-btn').click(function(){
+        var self = this;
+        var val = this.value;
+        data = val.split(',');
+
+        if(data[1] % 2 == 0){
+            assign(self);
+            $(this).attr('class', 'assign-btn btn btn-danger');
+            $(this).text('delete');
+            self.value = data[0] + ',' + (Number.parseInt(data[1]) + 1);
+        }else{
+            del(self);
+            $(this).attr('class', 'assign-btn btn btn-success');
+            $(this).text('assign');
+            self.value = data[0] + ',' + (Number.parseInt(data[1]) + 1);
+        }
+    });
+
+    $('.delete-btn').on("click", function(){
+        var self = this;
+        var val = this.value;
+        data = val.split(',');
+
+        if(data[1] % 2 == 0){
+            assign(self);
+            $(this).attr('class', 'delete-btn btn btn-danger');
+            $(this).text('delete');
+            self.value = data[0] + ',' + (Number.parseInt(data[1]) + 1);
+        }else{
+            del(self);
+            $(this).attr('class', 'delete-btn btn btn-success');
+            $(this).text('assign');
+            self.value = data[0] + ',' + (Number.parseInt(data[1]) + 1);
+        }
     });
 });
+
+
+
